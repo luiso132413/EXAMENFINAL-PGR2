871a2119c543faebbed21c543ddcb81080b4dce5
luiso132413 <loviedog@miumg.edu.gt>
2024-11-05 23:05:21 -0600

    test

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'antigua_umg2024_4441_ojy9',
+  username: 'antigua_umg2024_4441_user',
+  password: 'S0ONUblWWqbdfg99iOkufbxEXaBxEb2Z',
+  host: 'dpg-cslfeqhu0jms73f5rhv0-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
